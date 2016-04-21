9b49391d8a9515e04a83db7b4d9bd83660dfe35a
Tiago Calogero <t.calogero@digialta.com.br>
2016-04-21 17:28:17 -0300

    New repository :boom:

--- landing/src/js/app/controllers/HomeController.js
+++ landing/src/js/app/controllers/HomeController.js
@@ -1,44 +1,62 @@
 app.controller('HomeController', function($scope, $rootScope){
-	var init = function () {
-		// object to hold all the data for the new login form
-		$scope.valores = {};
-		$scope.outras = {};
-		$scope.loading = false;
+	var init = function(){
+		$scope.totalValue = 0;
+		$scope.days = 0;
+		$scope.values = {};
+		$scope.staticValue = 'static-200';
+		$scope.dynamicValue = 'dynamic-500';
+		$scope.other = {};
+		$scope.typeLanguages = [
+			{
+				"abb": "pt",
+				"title": "Portuguese"
+			},
+			{
+				"abb": "en",
+				"title": "English"
+			},
+			{
+				"abb": "es",
+				"title": "Spanish"
+			}
+		];
 	};
 
 	// fire on controller loaded
 	init();
 
-	// function to handle submitting the form
-	$scope.submitLogin = function() {
-		$scope.loading = true;
+	// Função disparada sempre que o objeto $scope.values e $scope.other sofrer alterações
+	$scope.$watch('[values, other]', function(newValues, oldValues){
+		$scope.totalValue = 0;
+
+		angular.forEach($scope.values, function(value, key){
+			$scope.totalValue += (value != 'NaN') ? parseFloat(value.split('-')[1]) : 0;
+		});
+
+		if($scope.service == 'site'){
+			if($scope.typeSite == 'template'){
+				$scope.days = 5;
+				$scope.totalValue += 500;
+
+				return;
+			}
+
+			angular.forEach($scope.other, function(value, key){
+				$scope.other.quantity = $scope.other.quantity || 1;
+				$scope.totalValue += (value != 'NaN') ? parseFloat($scope.other.value.split('-')[1] * $scope.other.quantity) : 0;
+			});
+		}
+	}, true);
+
+	$scope.selectService = function(service){
+		$scope.values = {};
+		$scope.other = {};
+		$scope.service = service;
 	};
 
-	$scope.$watchCollection('valores', function() {
-		// Função disparada sempre que o objeto $scope.valores sofrer alterações
-		$scope.valorTotal = 0;
-
-		angular.forEach($scope.valores, function(value, key) {
-			//alert(JSON.stringify(value));
-			$scope.valorTotal += parseFloat(value);
-			//$scope.valorTotal += parseFloat($scope.outras.valor * $scope.outras.qtd);
-		});
-	});
-
-	$scope.typeLanguages = [
-		{
-			"abb": "pt",
-			"title": "Portuguese"
-		},
-		{
-			"abb": "en",
-			"title": "English"
-		},
-		{
-			"abb": "es",
-			"title": "Espanish"
-		}
-	];
+	$scope.closePopup = function(){
+		$scope.close = true;
+	};
 
 	$scope.changeLanguage = function(lang){
 		$rootScope.language = lang;
